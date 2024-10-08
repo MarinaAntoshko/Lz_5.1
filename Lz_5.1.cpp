0e837eedf3d87597537f39ebf5fc2f4a6bef939b
топ <@DESKTOP-2P61BTM>
2024-10-08 10:46:37 +0300

    Добавьте файлы проекта.

---
+++ Lz_5.1.cpp
@@ -0,0 +1,42 @@
+﻿// 1. Знайти найбільший елемент одновимірного масиву.//
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int k;
+    int* arr;
+
+   
+    cout << "Розмір масиву: ";
+    cin >> k;
+    arr = new int[k];
+
+    cout << "Введіть " << k << " елементів масиву:" << endl;
+    for (int i = 0; i < k; i++) {
+        cout << "Елемент " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "Mасив:" << endl;
+    for (int i = 0; i < k; i++) {
+        cout << arr[i] << " ";
+    }
+
+    int maxElement = arr[0]; 
+    for (int i = 1; i < k; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+    cout << "\nНайбільший елемент масиву: " << maxElement << endl;
+
+    delete[] arr;
+
+    return 0;
+}
